70cef9b697c18d934a2acc2f5a340fa8e8965a74
yj.ahn <drinkjuice@naver.com>
2024-01-26 08:11:48 +0900

    장바구니 리스트 표시

--- lib/page/cart_page.dart
+++ lib/page/cart_page.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_mall/util/util.dart';
 
 import '../data/product_data.dart';
 import '../model/product.dart';
@@ -59,6 +61,64 @@ class _CartPageState extends State<CartPage> {
   Widget cartContainer((int, int) cart) {
     print(cart);
 
-    return Container();
+    var foundProduct = productList.firstWhere((product) => product.no == cart.$1);
+
+    return foundProduct == null
+        ? Container()
+        : Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CachedNetworkImage(
+                  imageUrl: foundProduct.imageUrl!,
+                  width: MediaQuery.of(context).size.width * .3,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(
+                      strokeWidth: 2,
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => Center(
+                    child: Text('오류 발생'),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8,),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        foundProduct.name!,
+                        textScaleFactor: 1.2,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text('${numberFormat.format(foundProduct.price)}'),
+                      Row(
+                        children: [
+                          Text('수량:',),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.remove),
+                          ),
+                          Text('${cart.$2}',),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.add),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.delete),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
