52f85e182df43dd948a8e69d9f2391d3013cf227
yj.ahn <drinkjuice@naver.com>
2024-01-26 08:28:24 +0900

    장바구니에서 리스트 표시하는 부분 복사

--- lib/page/cart_page.dart
+++ lib/page/cart_page.dart
@@ -23,19 +23,6 @@ class _CartPageState extends State<CartPage> {
     (4, 3),
   ];
 
-  List<Product> cartList2 = [
-    Product(
-        no: 1,
-        name: "노트북(Laptop)",
-        imageUrl: "https://picsum.photos/id/1/300/300",
-        price: 600000),
-    Product(
-        no: 4,
-        name: "키보드(Keyboard)",
-        imageUrl: "https://picsum.photos/id/60/300/300",
-        price: 50000),
-  ];
-
   double get totalPrice => cartList.fold(0.0, (total, cart) =>
       total + productList[cart.$1].price! * cart.$2);
 
--- lib/page/checkout_page.dart
+++ lib/page/checkout_page.dart
@@ -1,4 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
+import '../data/product_data.dart';
+import '../util/util.dart';
 
 class CheckoutPage extends StatefulWidget {
   const CheckoutPage({super.key});
@@ -8,18 +12,92 @@ class CheckoutPage extends StatefulWidget {
 }
 
 class _CheckoutPageState extends State<CheckoutPage> {
+  List<(int, int)> cartList = [
+    (1, 2),
+    (4, 3),
+  ];
+
+  double get totalPrice => cartList.fold(
+      0.0, (total, cart) => total + productList[cart.$1].price! * cart.$2);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('결재'),
+        title: Text('결재시작'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-
-        ],
+      body: ListView.builder(
+        itemCount: cartList.length,
+        itemBuilder: (context, index) => _checkoutContainer(cartList[index]),
       ),
     );
+  }
+
+  Widget _checkoutContainer((int, int) cart) {
+    print(cart);
+
+    var foundProduct = productList.firstWhere((product) =>
+    product.no == cart.$1);
+
+    return foundProduct == null
+        ? Container()
+        : Container(
+            padding: EdgeInsets.all(8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CachedNetworkImage(
+                  imageUrl: foundProduct.imageUrl!,
+                  width: MediaQuery.of(context).size.width * .3,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(
+                      strokeWidth: 2,
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => Center(
+                    child: Text('오류 발생'),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8,),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        foundProduct.name!,
+                        textScaleFactor: 1.2,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text('${numberFormat.format(foundProduct.price)}'),
+                      Row(
+                        children: [
+                          Text('수량:',),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.remove),
+                          ),
+                          Text('${cart.$2}',),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.add),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.delete),
+                          ),
+                        ],
+                      ),
+                      Text('합계: ${numberFormat.format(foundProduct.price! * cart.$2)}원',),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
