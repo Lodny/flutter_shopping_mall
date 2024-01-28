dbfb666f24fd3aedea591cce12abefcd3bf1725f
lodny <drinkjuice@naver.com>
2024-01-28 14:09:52 +0900

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_mall/page/product_list_page.dart';
 import 'package:flutter_shopping_mall/util/util.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized(); // 필수: 앱이 실행되기 전에 ensureInitialized() 호출
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initSharedPreferences();
 
   runApp(const MyApp());
